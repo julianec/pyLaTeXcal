ff0c821ee201ecf8c1ca6e6c9bc3f0820b59f3af
Juliane Clausen <julianeclausen@MacBook-Air.fritz.box>
2022-01-14 19:28:40 +0100

    Put most of the code into a class.

--- LaTeXCal.py
+++ LaTeXCal.py
@@ -15,41 +15,43 @@ latex_jinja_env = jinja2.Environment(
 	autoescape = False,
 	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
 )
-# create a new calendar object called my_calendar
-my_calendar = cal.Calendar()
-month_no = 1 # January
-month_name = cal.month_name[month_no] # January is at index 1! 0 = empty string
-year = 2022 # 2022
 
-# create string for blank days at the start of the month
-# add up to six '\BlankDay' to blankdays to pass to jinja
-def createBlankDayString(blankyear, blankmonth_no):
-	# create an iterator for the days of a given year and month as numbers 
-	# where days not belonging to the given month are 0.
-	month = my_calendar.itermonthdays(blankyear, blankmonth_no)
-	blankdays = ''
-	for day in month:
-		if day == 0:
-			blankdays = blankdays +'\BlankDay' + '\n'
-		else:
-			# we don't want '\BlankDay's for the days of the week after the end of the month
-			break
-	return blankdays
+class MonthlyOverview:
+	def __init__(self, month, year):
+		self.month_no = month
+		self.year = year
+		# create a new calendar object called my_calendar
+		self.my_calendar = cal.Calendar()
+		self.month_name = cal.month_name[self.month_no] # January is at index 1! 0 = empty string
+
+	# method to return a string for blank days at the start of the month
+	# add up to six '\BlankDay' to blankdays to pass to jinja
+	def getBlankDayString(self):
+		# create an iterator for the days of a given year and month as numbers 
+		# where days not belonging to the given month are 0.
+		month = self.my_calendar.itermonthdays(self.year, self.month_no)
+		blankdays = ''
+		for day in month:
+			if day == 0:
+				blankdays = blankdays +'\BlankDay' + '\n'
+			else:
+				# we don't want '\BlankDay's for the days of the week after the end of the month
+				break
+		return blankdays
 		 
-# create string for the days of the month
-def createDayString(daystringyear, daystringmonth_no): 
-	# create an iterator for the days of a given year and month as numbers 
-	# where days not belonging to the given month are 0.
-	month = my_calendar.itermonthdays(daystringyear, daystringmonth_no)
-	days = ''
-	for day in month:
-		if day != 0:
-			#days = days +'\day{}{\\vspace{2.5cm}} % day '+ str(day) + '\n'
-			days = days +'\day{}{\\vspace{2.0cm}} % day '+ str(day) + '\n'
-	return days
+	# method to return a string for the days of the month
+	def getDayString(self): 
+		# create an iterator for the days of a given year and month as numbers 
+		# where days not belonging to the given month are 0.
+		month = self.my_calendar.itermonthdays(self.year, self.month_no)
+		days = ''
+		for day in month:
+			if day != 0:
+				days = days +'\day{}{\\vspace{2.0cm}} % day '+ str(day) + '\n'
+		return days
 
-blankdays = createBlankDayString(year, month_no)
-days = createDayString(year, month_no)
+# create a monthly overview for January 2022
+myCal = MonthlyOverview(1, 2022)
 
 template = latex_jinja_env.get_template('calendar.tex.jinja2')
-print(template.render(month=month_name, year=year, blankdays=blankdays, days=days))
+print(template.render(month=myCal.month_name, year=myCal.year, blankdays=myCal.getBlankDayString(), days=myCal.getDayString()))
